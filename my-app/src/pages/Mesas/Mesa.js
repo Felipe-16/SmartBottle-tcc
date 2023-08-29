295337ead438a83367f328e2de81f5dcc4f85e22
Felipe Ros Pegini <felipe16699@gmail.com>
2023-08-28 23:48:35 -0300

    Recriando e componetizando os containers de mesa

--- my-app/src/pages/Mesas/Mesa.js
+++ my-app/src/pages/Mesas/Mesa.js
@@ -1,49 +1,58 @@
-import React, { Component } from "react";
-import ContainerMesa from './components/ContainerMesa'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import ContainerMesa from './components/ContainerMesa';
 
-const Container =styled.ul`
-    width: 90%;
-    margin: 0 auto;
-    padding: 50px 0;
-`
+const Mesa = ({ filtroLigado }) => {
+  // Dados das mesas
+  const mesas = [
+    { title: "Mesa 01", nomeChip: "Atendido", temperature: "25", status: "Cheia", ligado: 1 },
+    { title: "Mesa 02", nomeChip: "Atender Mesa", temperature: "25", status: "Cheia", ligado: 1 },
+    { title: "Mesa 03", nomeChip: "Atendido", temperature: "25", status: "Vazio", ligado: 1 },
+    { title: "Mesa 04", nomeChip: "Atendido", temperature: "25", status: "Cheia", ligado: 1 },
+    { title: "Mesa 09", nomeChip: "Atender Mesa", temperature: "25", status: "Cheia", ligado: 1 },
+    { title: "Mesa 10", nomeChip: "Atender Mesa", temperature: "25", status: "Metade", ligado: 1 },
+    { title: "Mesa 05", ligado: 0 },
+    { title: "Mesa 06", ligado: 0 },
+    { title: "Mesa 07", ligado: 0 },
+    { title: "Mesa 08", ligado: 0 },
+    { title: "Mesa 11", ligado: 0 },
+    { title: "Mesa 12", ligado: 0 },
+    { title: "Mesa 13", ligado: 0 },
+  ];
 
-function Mesa () {
+  // Filtra as mesas de acordo com o filtroLigado
+  const mesasFiltradas = mesas.filter(mesa => mesa.ligado === filtroLigado);
 
-    // const [realocacoes, setRealocacoes] = useState([])
+  return (
+    <CardContainer>
+      {mesasFiltradas.map(mesa => (
+        <ContainerMesa
+          key={mesa.title}
+          title={mesa.title}
+          nomeChip={mesa.nomeChip}
+          temperature={mesa.temperature}
+          status={mesa.status}
+          ligado={mesa.ligado}
+        />
+      ))}
+    </CardContainer>
+  );
+};
 
-    // useEffect(() => {
-    //     axios.get("http://localhost:6000/realocacao")
-    //         .then(((response) => {
-    //             setDoacoes(response.data)
-    //         }))
-    //         .catch(() => {
-    //             console.log('deu errado')
-    //         })
-    // }, [])
+const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 28px;
+  margin-bottom: 20px;
+  justify-content: flex-start;
 
+  width: calc(100% - 104px);
+  margin-left: 104px;
+`;
 
-        return (
-            <Container>
-                <ContainerMesa
-                    mesa={"Numero da Mesa: 1"}
-                    temperatura = {"20"}
-                    status={"Cheia"}
-                />
-                <ContainerMesa
-                    mesa={"Numero da Mesa: 2"}
-                    temperatura = {"20"}
-                    status={"Cheia"}
-                />
-                <ContainerMesa
-                    mesa={"Numero da Mesa: 3"}
-                    temperatura = {"20"}
-                    status={"Cheia"}
-                />
-            </Container>
-
-        )
-    }
+export default Mesa;
 
 
-export default Mesa
+
+
+
--- my-app/src/pages/Mesas/components/ContainerMesa.js
+++ my-app/src/pages/Mesas/components/ContainerMesa.js
@@ -1,68 +1,180 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ContainerMesa = ({ mesa, temperatura, status }) => {
-  return (
-    <CaixaLi>
-      <DivTitulo>
-        <Titulo>{mesa}</Titulo>
-      </DivTitulo>
-      <Media />
-      <CaixaTexto>
-        <DescricaoMesas>Temp: {temperatura}</DescricaoMesas>
-        <DescricaoMesas>Status: {status}</DescricaoMesas>
-      </CaixaTexto>
-    </CaixaLi>
-  );
+const ContainerMesa = ({ svg, title, nomeChip, temperature, status, ligado }) => {
+  const isAtendido = nomeChip === 'Atendido';
+  const isLigado = ligado ===1;
+  
+
+  if (ligado === 1) {
+    return (
+      <CardWrapperLigado>
+        <SvgContainer>
+          <StatusSvg status={status}></StatusSvg>
+        </SvgContainer>
+        <ContentLigado>
+          <Title>{title}</Title>
+          <ChipStatus atendido={isAtendido}>
+            <Text>{nomeChip}</Text>
+          </ChipStatus>
+          <Info>
+            <p>Status da bebida: {status}</p>
+            <p>Temperaturada bebida: {temperature}°C</p>
+          </Info>
+        </ContentLigado>
+      </CardWrapperLigado>
+    );
+  } else if (ligado === 0) {
+    return (
+      <CardWrapperDesligado>
+        <SvgContainer>
+          <SvgNada />
+        </SvgContainer>
+        <ContentDesligado>
+          <Title>{title}</Title>
+        </ContentDesligado>
+      </CardWrapperDesligado>
+    );
+  } else {
+    return null; // Caso ligado não seja 0 nem 1
+  }
 };
 
 export default ContainerMesa;
 
-const CaixaLi = styled.li`
-  display: inline-block;
-  list-style: none;
-  width: 30%;
-  vertical-align: top;
-  margin: 1.5% 1.5%;
-  box-sizing: border-box;
-  box-shadow: 0px 2px 23px #26395338;
-  border-radius: 25px;
+
+const CardWrapperLigado = styled.div`
+  width: 407px;
+  height: 198px;
+  display: flex;
+  background-color: #FFFFFF;
+  border-radius: 20px;
+  margin-bottom: 20px;
+  flex-shrink: 0;
+  box-shadow: 4px 4px 4px 2px rgba(0, 0, 0, 0.25);
 `;
 
-const DivTitulo = styled.div`
-  background-color: gray;
-  padding: 0;
-  text-align: center;
+const CardWrapperDesligado = styled.div`
+  width: 407px;
+  height: 70px;
+  display: flex;
+  background-color: #969D9D;
+  border-radius: 20px;
+  margin-bottom: 20px;
+  flex-shrink: 0;
+  box-shadow: 4px 4px 4px 2px rgba(0, 0, 0, 0.25);
 `;
 
-const Titulo = styled.h2`
+const SvgContainer = styled.div`
+  width: 20px;
+  padding: 10px;
+`;
+
+
+const StatusSvg = ({ status }) => {
+  if (status === 'Cheia') {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="177" viewBox="0 0 20 177" fill="none">
+        <rect y="0.31427" width="20" height="175.371" rx="10" fill="#DBE3F0" />
+        <rect y="0.879974" width="20" height="175.371" rx="10" fill="url(#paint0_linear_39_297)" />
+        <defs>
+          <linearGradient id="paint0_linear_39_297" x1="10" y1="0.879974" x2="10" y2="176.251" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FFC700" />
+            <stop offset="0.9999" stop-color="#765C00" />
+            <stop offset="1" stop-color="#C19808" stop-opacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    );
+  } else if (status === 'Metade') {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="185" viewBox="0 0 28 185" fill="none">
+        <rect x="4" y="0.31427" width="20" height="175.371" rx="10" fill="#DBE3F0" />
+        <g filter="url(#filter0_d_39_395)">
+          <rect x="4" y="88.5657" width="20" height="87.6857" rx="10" fill="url(#paint0_linear_39_395)" shape-rendering="crispEdges" />
+        </g>
+        <defs>
+          <filter id="filter0_d_39_395" x="0" y="88.5657" width="28" height="95.6857" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_39_395" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_39_395" result="shape" />
+          </filter>
+          <linearGradient id="paint0_linear_39_395" x1="14" y1="88.5657" x2="14" y2="176.251" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FFC700" />
+            <stop offset="0.9999" stop-color="#765C00" />
+            <stop offset="1" stop-color="#C19808" stop-opacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    );
+  } else if (status === 'Vazio') {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="176" viewBox="0 0 20 176" fill="none">
+        <rect y="0.31427" width="20" height="175.371" rx="10" fill="#DBE3F0" />
+      </svg>
+    );
+  } else {
+    return null; // Caso o status não seja um dos valores acima
+  }
+};
+
+const SvgNada = styled.div`
+  width: 20px;
+  height: 49.412px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  background: var(--white-2, #DBE3F0);  
+  `
+
+const ContentLigado = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ContentDesligado = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h2`
   margin: 0;
-  padding: 10px;
-  font-weight: bolder;
-  color: white; /* Cor do texto do título */
+  color: #000000;
+  font-family: Montserrat;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
 `;
 
-const Media = styled.div`
-  background: url("https://th.bing.com/th/id/OIP.BMWsryGKGD_9dVViNV1a3AHaE6?pid=ImgDet&rs=1") center/cover;
-  height: 150px;
-  width: auto;
+const ChipStatus = styled.div`
+  display: flex;
+  padding: 12px 20px;
+  width: 126px;
+  height: 20px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 20px;
+  background: ${props => (props.atendido ? '#2CB44E' : '#FFE500')};
+  
 `;
 
-const CaixaTexto = styled.div`
-  background-color: #F0F8FF; /* Cor de fundo das informações */
-  padding: 10px;
-  border-radius: 0 0 5px 5px;
+const Text = styled.p`
+  color: var(--black, #000);
+  font-family: Montserrat;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
 `;
 
-// const DescricaoAbrigo = styled.p`
-//   font-size: var(--descricao);
-//   color: var(--primary);
-//   margin-bottom: 10px;
-// `;
+const Info = styled.div``;
 
-const DescricaoMesas = styled.p`
-  font-size: 24px; /* Mantém o tamanho original */
-  color: black;
-  margin-bottom: 10px;
-  font-family: "Roboto";
-`;
