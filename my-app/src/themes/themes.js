7adc636c287a68f338618d14b541b8a14db684f3
Felipe Ros Pegini <felipe16699@gmail.com>
2023-09-09 21:33:56 -0300

    inclusão de arquivos para themeMode

---
+++ my-app/src/themes/themes.js
@@ -0,0 +1,11 @@
+  export const lightTheme = {
+    backgroundColor: "#FAFAFA",
+    textColor: "#000000",
+    containerColor: "#FFFFFF"
+  };
+
+  export const darkTheme = {
+    backgroundColor: "#151C2D",
+    textColor: "#ffffff",
+    containerColor: "#0F141E"
+  }
